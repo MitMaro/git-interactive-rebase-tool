aeb28c7a46957b87698148c7a26345f686e0d0b8
Tim Oram <dev@mitmaro.ca>
2019-03-11 20:33:04 -0230

    Combine git_config with config
    There was a plan where these two files would have distinct purposes but
    that does not seem to be the case any longer. So this change combines
    the config and git_config modules into a single module.

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use color::Color;
-use git_config::GitConfig;
 use std::ffi::OsString;
+use std::env;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -21,28 +21,75 @@ pub struct Config {
 	pub editor: OsString,
 }
 
-fn string_to_color(color_string: &str, default_color: Color) -> Color {
-	Color::try_from(color_string).unwrap_or(default_color)
+fn get_string(config: &git2::Config, name: &str, default: &str) -> Result<String, String> {
+	match config.get_string(name) {
+		Ok(v) => Ok(v),
+		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(String::from(default)),
+		Err(e) => Err(format!("Error reading git config: {}", e)),
+	}
+}
+
+fn get_os_string(config: &git2::Config, name: &str, default: OsString) -> Result<OsString, String> {
+	match config.get_string(name) {
+		Ok(v) => Ok(OsString::from(v)),
+		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default),
+		Err(e) => Err(format!("Error reading git config: {}", e)),
+	}
+}
+
+fn get_bool(config: &git2::Config, name: &str, default: bool) -> Result<bool, String> {
+	match config.get_bool(name) {
+		Ok(v) => Ok(v),
+		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default),
+		Err(e) => Err(format!("Error reading git config: {}", e)),
+	}
+}
+
+fn get_color(config: &git2::Config, name: &str, default_color: Color) -> Result<Color, String> {
+	match config.get_string(name) {
+		Ok(v) => Color::try_from(v.to_lowercase().as_str()),
+		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default_color),
+		Err(e) => Err(format!("Error reading git config: {}", e)),
+	}
+}
+
+fn editor_from_env() -> OsString {
+	env::var_os("VISUAL")
+		.or_else(|| env::var_os("EDITOR"))
+		.unwrap_or_else(|| OsString::from("vi"))
+}
+
+fn open_git_config() -> Result<git2::Config, String> {
+	match git2::Repository::open_from_env() {
+		Ok(f) => {
+			match f.config() {
+				Ok(c) => Ok(c),
+				Err(e) => Err(format!("Error reading git config: {}", e))
+			}
+		},
+		Err(e) => Err(format!("Error reading git config: {}", e)),
+	}
 }
 
 impl Config {
-	pub fn new(git_config: GitConfig) -> Self {
-		Config {
-			comment_char: git_config.comment_char,
-			foreground_color: string_to_color(git_config.foreground_color.as_ref(), Color::White),
-			indicator_color: string_to_color(git_config.indicator_color.as_ref(), Color::Yellow),
-			error_color: string_to_color(git_config.error_color.as_ref(), Color::Red),
-			diff_add_color: string_to_color(git_config.diff_add_color.as_ref(), Color::Green),
-			diff_remove_color: string_to_color(git_config.diff_add_color.as_ref(), Color::Red),
-			pick_color: string_to_color(git_config.pick_color.as_ref(), Color::Green),
-			reword_color: string_to_color(git_config.reword_color.as_ref(), Color::Yellow),
-			edit_color: string_to_color(git_config.edit_color.as_ref(), Color::Blue),
-			exec_color: string_to_color(git_config.edit_color.as_ref(), Color::White),
-			squash_color: string_to_color(git_config.squash_color.as_ref(), Color::Cyan),
-			fixup_color: string_to_color(git_config.fixup_color.as_ref(), Color::Magenta),
-			drop_color: string_to_color(git_config.drop_color.as_ref(), Color::Red),
-			auto_select_next: git_config.auto_select_next,
-			editor: git_config.editor,
-		}
+	pub fn new() -> Result<Self, String> {
+		let git_config = open_git_config()?;
+		Ok(Config {
+			comment_char: get_string(&git_config, "core.commentChar", "#")?,
+			foreground_color: get_color(&git_config, "interactive-rebase-tool.foregroundColor", Color::White)?,
+			indicator_color: get_color(&git_config, "interactive-rebase-tool.indicatorColor", Color::Cyan)?,
+			error_color: get_color(&git_config, "interactive-rebase-tool.errorColor", Color::Red)?,
+			diff_add_color: get_color(&git_config, "interactive-rebase-tool.diffAddColor", Color::Green)?,
+			diff_remove_color: get_color(&git_config, "interactive-rebase-tool.diffRemoveColor", Color::Red)?,
+			pick_color: get_color(&git_config, "interactive-rebase-tool.pickColor", Color::Green)?,
+			reword_color: get_color(&git_config, "interactive-rebase-tool.rewordColor", Color::Yellow)?,
+			edit_color: get_color(&git_config, "interactive-rebase-tool.editColor", Color::Blue)?,
+			exec_color: get_color(&git_config, "interactive-rebase-tool.execColor", Color::White)?,
+			squash_color: get_color(&git_config, "interactive-rebase-tool.squashColor", Color::Cyan)?,
+			fixup_color: get_color(&git_config, "interactive-rebase-tool.fixupColor", Color::Magenta)?,
+			drop_color: get_color(&git_config, "interactive-rebase-tool.dropColor", Color::Red)?,
+			auto_select_next: get_bool(&git_config, "interactive-rebase-tool.autoSelectNext", false)?,
+			editor: get_os_string(&git_config, "core.editor", editor_from_env())?,
+		})
 	}
 }
