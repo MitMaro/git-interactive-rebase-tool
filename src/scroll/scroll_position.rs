35c5a4340945fe949fac922e833340622e2ed098
Tim Oram <dev@mitmaro.ca>
2019-04-21 21:47:41 -0230

    Refactor scroll position for the main list view

--- src/scroll/scroll_position.rs
+++ src/scroll/scroll_position.rs
@@ -29,6 +29,29 @@ impl ScrollPosition {
 		self.update_top(false, window_height, lines_length);
 	}
 
+	pub fn ensure_cursor_visible(&self, cursor: usize, window_height: usize, lines_length: usize) {
+		let view_height = window_height as usize - self.padding;
+
+		let current_value = *self.value.borrow();
+
+		// TODO I think this can be simplified
+		self.value.replace(match cursor {
+			// show all if list is view height is long enough
+			_ if lines_length <= view_height => 0,
+			// last item selected, set top to show bottom of lines
+			s if s >= lines_length - 1 => lines_length - view_height,
+			// if on top two of list set top to top of list
+			s if s < 1 => 0,
+			// if selected item is hidden above top, shift top up
+			s if s < current_value => s,
+			// if starting scrolling, hide top two
+			s if current_value == 0 && s >= view_height => 1,
+			// if selected item is hidden below, shift top down
+			s if s >= current_value + view_height => s - view_height + 1,
+			_ => current_value,
+		});
+	}
+
 	pub fn get_position(&self) -> usize {
 		*self.value.borrow()
 	}
