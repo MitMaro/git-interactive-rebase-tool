0d565b5cae7b8aed4578e266c3c3375772d75564
Tim Oram <dev@mitmaro.ca>
2019-07-22 22:20:09 -0230

    Finish adding keybindings
    A few key bindings were incomplete and the help did not respect the
    custom keybindings.

---
+++ src/input/utils.rs
@@ -0,0 +1,15 @@
+use pancurses::Input;
+
+pub fn curses_input_to_string(input: Input) -> String {
+	match input {
+		Input::Character(c) => c.to_string(),
+		Input::KeyLeft => String::from("Left"),
+		Input::KeyRight => String::from("Right"),
+		Input::KeyDown => String::from("Down"),
+		Input::KeyUp => String::from("Up"),
+		Input::KeyPPage => String::from("PageUp"),
+		Input::KeyNPage => String::from("PageDown"),
+		Input::KeyResize => String::from("Resize"),
+		_ => String::from("Other"),
+	}
+}
