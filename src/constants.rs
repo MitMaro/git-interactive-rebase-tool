d62b665b3c814ee477b6966a4ac4e2e355299330
Rustin-Liu <rustin.liu@gmail.com>
2019-10-12 18:58:57 +0800

    fix constants.rs comment style problem

--- src/constants.rs
+++ src/constants.rs
@@ -10,7 +10,7 @@ pub const SHORT_ERROR_MESSAGE: &str = "Window too small\n";
 pub const SHORT_ERROR_MESSAGE_WIDTH: usize = 16;
 
 pub const MINIMUM_WINDOW_HEIGHT: usize = 5; // title + pad top + line + pad bottom + help
-pub const MINIMUM_COMPACT_WINDOW_WIDTH: usize = 20; //">s ccc mmmmmmmmmmmmm".len()
+pub const MINIMUM_COMPACT_WINDOW_WIDTH: usize = 20; // ">s ccc mmmmmmmmmmmmm".len()
 pub const MINIMUM_FULL_WINDOW_WIDTH: usize = 34; // " > squash cccccccc mmmmmmmmmmmmm %".len()
 
 pub const NAME: &str = "interactive-rebase-tool";
