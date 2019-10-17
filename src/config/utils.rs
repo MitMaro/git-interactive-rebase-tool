73c4fe362720cb170024cd1c3c04a42faed52d86
Tim Oram <dev@mitmaro.ca>
2019-10-17 20:03:05 -0230

    Move Color into the display module

--- src/config/utils.rs
+++ src/config/utils.rs
@@ -1,4 +1,4 @@
-use crate::color::Color;
+use crate::display::Color;
 use std::convert::TryFrom;
 use std::env;
 
--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,9 +1,11 @@
+mod color;
 mod color_manager;
 mod curses;
 #[allow(clippy::module_inception)]
 mod display;
 mod display_color;
 
+pub use self::color::Color;
 pub use self::curses::Curses;
 pub use self::display::Display;
 pub use self::display_color::DisplayColor;
