5451d7313ad763aab74a181dff51920f5290744c
Tim Oram <dev@mitmaro.ca>
2019-10-17 21:40:26 -0230

    Remove disabled clippy rule

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -6,4 +6,4 @@ set -o pipefail
 
 rustup update stable
 rustup component add clippy --toolchain stable
-cargo +stable clippy --all-features -- -D warnings -A clippy::new_ret_no_self
+cargo +stable clippy --all-features -- -D warnings
