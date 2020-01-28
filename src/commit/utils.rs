4912a27c9f4e312c03fe893631a945348b8779d6
Tim Oram <dev@mitmaro.ca>
2020-01-27 22:20:38 -0330

    Add tests and docs for commit::utils

--- src/commit/utils.rs
+++ src/commit/utils.rs
@@ -4,6 +4,7 @@ use crate::commit::Commit;
 use chrono::{Local, TimeZone};
 use git2::{Delta, DiffFindOptions, DiffOptions, Error, Repository};
 
+/// Load commit information from a commit hash.
 pub(super) fn load_commit_state(hash: &str) -> Result<Commit, Error> {
 	let repo = Repository::open_from_env()?;
 	let commit = repo.find_commit(repo.revparse_single(hash)?.id())?;
@@ -74,4 +75,126 @@ pub(super) fn load_commit_state(hash: &str) -> Result<Commit, Error> {
 	};
 
 	Ok(Commit::new(full_hash, author, committer, date, file_stats, body))
+}
+
+#[cfg(test)]
+mod tests {
+	// some of this file is difficult to test because it would require a non-standard git repo, so
+	// we test what is possible
+	use crate::commit::utils::load_commit_state;
+	use git2::Delta;
+	use std::env::set_var;
+	use std::path::Path;
+
+	fn set_git_dir(fixture: &str) {
+		set_var(
+			"GIT_DIR",
+			Path::new(env!("CARGO_MANIFEST_DIR"))
+				.join("test")
+				.join("fixtures")
+				.join(fixture)
+				.to_str()
+				.unwrap(),
+		);
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_hash() {
+		set_git_dir("simple");
+		let commit = load_commit_state("18d82dcc4c36cade807d7cf79700b6bbad8080b9").unwrap();
+		assert_eq!(commit.hash, "18d82dcc4c36cade807d7cf79700b6bbad8080b9");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_author() {
+		set_git_dir("simple");
+		let commit = load_commit_state("18d82dcc4c36cade807d7cf79700b6bbad8080b9").unwrap();
+		assert_eq!(commit.get_author().to_string().unwrap(), "Tim Oram <dev@mitmaro.ca>");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_date() {
+		set_git_dir("simple");
+		let commit = load_commit_state("18d82dcc4c36cade807d7cf79700b6bbad8080b9").unwrap();
+		assert_eq!(commit.get_date().timestamp(), 1580172067);
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_body() {
+		set_git_dir("simple");
+		let commit = load_commit_state("18d82dcc4c36cade807d7cf79700b6bbad8080b9").unwrap();
+		assert_eq!(
+			commit.get_body().as_ref().unwrap(),
+			"Empty commit title\n\nEmpty commit body\n"
+		);
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_committer_match_author() {
+		set_git_dir("simple");
+		let commit = load_commit_state("ac950e31a96660e55d8034948b5d9b985c97692d").unwrap();
+		assert!(commit.get_committer().to_string().is_none());
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_committer_not_match_author() {
+		set_git_dir("simple");
+		let commit = load_commit_state("2836dcdcbd040f9157652dd3db0d584a44d4793d").unwrap();
+		assert_eq!(
+			commit.get_committer().to_string().unwrap(),
+			"Not Tim Oram <not-dev@mitmaro.ca>"
+		);
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_modified_file() {
+		set_git_dir("simple");
+		let commit = load_commit_state("1cc0456637cb220155e957c641f483e60724c581").unwrap();
+		let file_stat = commit.get_file_stats().as_ref().unwrap().first().unwrap();
+		// 		file_stat.get_status()
+		assert_eq!(*file_stat.get_status(), Delta::Modified);
+		assert_eq!(file_stat.get_from_name(), "a");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_added_file() {
+		set_git_dir("simple");
+		let commit = load_commit_state("c1ac7f2c32f9e00012f409572d223c9457ae497b").unwrap();
+		let file_stat = commit.get_file_stats().as_ref().unwrap().first().unwrap();
+		// 		file_stat.get_status()
+		assert_eq!(*file_stat.get_status(), Delta::Added);
+		assert_eq!(file_stat.get_from_name(), "e");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_deleted_file() {
+		set_git_dir("simple");
+		let commit = load_commit_state("d85479638307e4db37e1f1f2c3c807f7ff36a0ff").unwrap();
+		let file_stat = commit.get_file_stats().as_ref().unwrap().first().unwrap();
+		// 		file_stat.get_status()
+		assert_eq!(*file_stat.get_status(), Delta::Deleted);
+		assert_eq!(file_stat.get_from_name(), "b");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_renamed_file() {
+		set_git_dir("simple");
+		let commit = load_commit_state("aed0fd1db3e73c0e568677ae8903a11c5fbc5659").unwrap();
+		let file_stat = commit.get_file_stats().as_ref().unwrap().first().unwrap();
+		// 		file_stat.get_status()
+		assert_eq!(*file_stat.get_status(), Delta::Renamed);
+		assert_eq!(file_stat.get_from_name(), "c");
+		assert_eq!(file_stat.get_to_name(), "f");
+	}
+
+	#[test]
+	fn commit_utils_load_commit_state_load_copied_file() {
+		set_git_dir("simple");
+		let commit = load_commit_state("c028f42bdb2a5a9f80adea23d95eb240b994a6c2").unwrap();
+		let file_stat = commit.get_file_stats().as_ref().unwrap().first().unwrap();
+		// 		file_stat.get_status()
+		assert_eq!(*file_stat.get_status(), Delta::Copied);
+		assert_eq!(file_stat.get_from_name(), "d");
+		assert_eq!(file_stat.get_to_name(), "g");
+	}
 }
