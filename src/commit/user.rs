34a90c82c5e8524870507c903a1027752ae5008b
Tim Oram <dev@mitmaro.ca>
2020-01-29 20:33:51 -0330

    Add tests and docs for commit::user

--- src/commit/user.rs
+++ src/commit/user.rs
@@ -1,3 +1,4 @@
+/// Represents a user within a commit with a name and email address
 #[derive(Debug, Eq, PartialEq)]
 pub(crate) struct User {
 	name: Option<String>,
@@ -5,6 +6,7 @@ pub(crate) struct User {
 }
 
 impl User {
+	/// Creates a new user
 	pub(super) fn new(name: Option<&str>, email: Option<&str>) -> Self {
 		User {
 			email: email.map(String::from),
@@ -12,6 +14,10 @@ impl User {
 		}
 	}
 
+	/// Creates a formatted string of the user
+	///
+	/// The user if formatted with "Name \<Email\>", which matches the Git CLI. If name or email are
+	/// `None` then they are omitted from the result. If neither are set, `None` is returned.
 	pub(crate) fn to_string(&self) -> Option<String> {
 		let name = &self.name;
 		let email = &self.email;
@@ -29,5 +35,63 @@ impl User {
 				}
 			},
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::commit::user::User;
+
+	#[test]
+	fn commit_user_with_none_name_email() {
+		let user = User::new(None, None);
+		assert!(user.to_string().is_none());
+	}
+
+	#[test]
+	fn commit_user_with_none_name_with_email() {
+		let user = User::new(None, Some("me@example.com"));
+		assert_eq!(user.to_string().unwrap(), "<me@example.com>");
+	}
+
+	#[test]
+	fn commit_user_with_name_none_email() {
+		let user = User::new(Some("Tim Oram"), None);
+		assert_eq!(user.to_string().unwrap(), "Tim Oram");
+	}
+
+	#[test]
+	fn commit_user_with_name_email() {
+		let user = User::new(Some("Tim Oram"), Some("me@example.com"));
+		assert_eq!(user.to_string().unwrap(), "Tim Oram <me@example.com>");
+	}
+
+	#[test]
+	fn commit_user_compare_users_matching_name_email() {
+		assert_eq!(
+			User::new(Some("Tim Oram"), Some("me@example.com")),
+			User::new(Some("Tim Oram"), Some("me@example.com"))
+		);
+	}
+
+	#[test]
+	fn commit_user_compare_users_matching_name_only() {
+		assert_ne!(
+			User::new(Some("Tim Oram"), Some("me1@example.com")),
+			User::new(Some("Tim Oram"), Some("me2@example.com"))
+		);
+	}
+
+	#[test]
+	fn commit_user_compare_users_matching_email_only() {
+		assert_ne!(
+			User::new(Some("Tim Oram 1"), Some("me@example.com")),
+			User::new(Some("Tim Oram 2"), Some("me@example.com"))
+		);
+	}
+
+	#[test]
+	fn commit_user_compare_users_matching_none() {
+		assert_eq!(User::new(None, None), User::new(None, None));
 	}
 }
