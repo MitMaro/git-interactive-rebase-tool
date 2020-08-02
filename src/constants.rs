38ac9846273c9c5d9af4d46bb76d39681c3a8dc5
Tim Oram <dev@mitmaro.ca>
2020-08-02 19:05:57 -0230

    Move Window Size Error module to use ViewData
    This updates the Window Size Error module to use the ViewData struct
    over a direct view render.

--- src/constants.rs
+++ src/constants.rs
@@ -4,10 +4,7 @@ pub const TITLE_SHORT: &str = "Git Rebase";
 pub const TITLE_SHORT_LENGTH: usize = 10;
 pub const TITLE_HELP_INDICATOR_LENGTH: usize = 6;
 
-pub const HEIGHT_ERROR_MESSAGE: &str = "Window too small, increase height to continue\n";
 pub const MINIMUM_WINDOW_HEIGHT_ERROR_WIDTH: usize = 45;
-pub const SHORT_ERROR_MESSAGE: &str = "Window too small\n";
-pub const SHORT_ERROR_MESSAGE_WIDTH: usize = 16;
 
 pub const MINIMUM_WINDOW_HEIGHT: usize = 5; // title + pad top + line + pad bottom + help
 pub const MINIMUM_COMPACT_WINDOW_WIDTH: usize = 20; // ">s ccc mmmmmmmmmmmmm".len()
