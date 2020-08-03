d1d6a42809fbc7a5a007d8c239f9c8e8fe27bda6
Tim Oram <dev@mitmaro.ca>
2020-08-03 20:11:43 -0230

    Refactor Edit module to use ViewData
    The Edit module is the final module that is not using the ViewData. This
    adds a raw content value to the ViewData that will generated wrapped
    content lines. The Edit module uses this new functionality to remove the
    raw renders from the module.

--- src/edit/mod.rs
+++ src/edit/mod.rs
@@ -6,6 +6,9 @@ use crate::process::handle_input_result::HandleInputResult;
 use crate::process::process_module::ProcessModule;
 use crate::process::process_result::{ProcessResult, ProcessResultBuilder};
 use crate::process::state::State;
+use crate::view::line_segment::LineSegment;
+use crate::view::view_data::ViewData;
+use crate::view::view_line::ViewLine;
 use crate::view::View;
 use unicode_segmentation::UnicodeSegmentation;
 
@@ -19,6 +22,7 @@ pub struct Edit {
 	content: String,
 	cursor_position: usize,
 	state: EditState,
+	view_data: ViewData,
 }
 
 impl ProcessModule for Edit {
@@ -48,8 +52,8 @@ impl ProcessModule for Edit {
 	fn handle_input(
 		&mut self,
 		input_handler: &InputHandler<'_>,
-		_git_interactive: &mut GitInteractive,
-		_view: &View<'_>,
+		_: &mut GitInteractive,
+		view: &View<'_>,
 	) -> HandleInputResult
 	{
 		if self.state == EditState::Finish {
@@ -107,6 +111,10 @@ impl ProcessModule for Edit {
 					}
 				},
 				Input::Enter => self.state = EditState::Finish,
+				Input::Resize => {
+					let (view_width, view_height) = view.get_view_size();
+					self.view_data.set_view_size(view_width, view_height);
+				},
 				_ => {
 					continue;
 				},
@@ -116,45 +124,56 @@ impl ProcessModule for Edit {
 		HandleInputResult::new(input)
 	}
 
-	fn render(&self, view: &View<'_>, _git_interactive: &GitInteractive) {
-		let line = self.content.as_str();
-		let pointer = self.cursor_position;
-
-		view.draw_title(false);
-		view.set_style(false, true, false);
-		view.set_color(DisplayColor::Normal, false);
-
-		// this could probably be made way more efficient
-		let graphemes = UnicodeSegmentation::graphemes(line, true);
-		let segment_length = graphemes.clone().count();
-		for (counter, c) in graphemes.enumerate() {
-			if counter == pointer {
-				view.set_style(false, true, false);
-				view.draw_str(c);
-				view.set_style(false, false, false);
-			}
-			else {
-				view.draw_str(c);
-			}
-		}
-		if pointer >= segment_length {
-			view.set_style(false, true, false);
-			view.draw_str(" ");
-			view.set_style(false, false, false);
-		}
-
-		view.draw_str("\n\n");
-		view.set_color(DisplayColor::IndicatorColor, false);
-		view.draw_str("Enter to finish");
-	}
+	fn render(&self, _: &View<'_>, _: &GitInteractive) {}
 }
 
 impl Edit {
 	pub(crate) fn new() -> Self {
+		let mut view_data = ViewData::new();
+		view_data.set_show_title(true);
 		Self {
 			content: String::from(""),
 			cursor_position: 0,
 			state: EditState::Active,
+			view_data,
 		}
+	}
+
+	pub(crate) fn build_view_data(&mut self, view: &View<'_>, _: &GitInteractive) -> &ViewData {
+		let (view_width, view_height) = view.get_view_size();
+
+		let line = self.content.as_str();
+		let pointer = self.cursor_position;
+
+		let graphemes = UnicodeSegmentation::graphemes(line, true);
+
+		let start = graphemes.clone().take(pointer).collect::<String>();
+		let indicator = graphemes.clone().skip(pointer).take(1).collect::<String>();
+		let end = graphemes.skip(pointer + 1).collect::<String>();
+
+		let mut segments = vec![
+			LineSegment::new(start.as_str()),
+			LineSegment::new_with_color_and_style(indicator.as_str(), DisplayColor::Normal, false, true, false),
+			LineSegment::new(end.as_str()),
+		];
+		if end.is_empty() {
+			segments.push(LineSegment::new_with_color_and_style(
+				" ",
+				DisplayColor::Normal,
+				false,
+				true,
+				false,
+			));
+		}
+		self.view_data.clear();
+		self.view_data.set_content(ViewLine::new(segments));
+		self.view_data
+			.push_trailing_line(ViewLine::new(vec![LineSegment::new_with_color(
+				"Enter to finish",
+				DisplayColor::IndicatorColor,
+			)]));
+		self.view_data.set_view_size(view_width, view_height);
+		self.view_data.rebuild();
+		&self.view_data
 	}
 }
