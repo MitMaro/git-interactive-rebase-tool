5ed35b28a14d64aec8c33869d8884dbce2902889
Tim Oram <dev@mitmaro.ca>
2020-09-17 12:08:52 -0230

    Update linting script
    Add all targets to the linting script to ensure that the tests are also
    being linted. Also add an optional linting run against the latest
    nightly version of Clippy, but allow it to fail "successfully".

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -6,4 +6,11 @@ set -o pipefail
 
 rustup update stable
 rustup component add clippy --toolchain stable
-cargo +stable clippy --all-features
+cargo +stable clippy --all-targets --all-features
+
+# try to run the nightly version, if it exists, but do not fail
+(
+	rustup update nightly && \
+	rustup component add clippy --toolchain nightly && \
+	cargo +nightly clippy --all-targets --all-features
+) || true
