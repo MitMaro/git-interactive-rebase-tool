86e94b13f17bc9f26c697805da25421970f68f65
Tim Oram <dev@mitmaro.ca>
2020-09-23 12:46:09 -0230

    Break out lint and coverage from Linux tests in CI
    This moves the lint/format and coverage tests into their own jobs in the
    GitHub actions. This will hopefully speed up the GitHub actions run for
    pull requests.

--- scripts/coverage.bash
+++ scripts/coverage.bash
@@ -4,5 +4,6 @@ set -e
 set -u
 set -o pipefail
 
+rustup update nightly
 cargo +nightly install cargo-tarpaulin
 cargo +nightly tarpaulin --exclude-files=src/display/ncurses.rs --all-features --ignore-tests --line --verbose --out Html --out Lcov --output-dir coverage
