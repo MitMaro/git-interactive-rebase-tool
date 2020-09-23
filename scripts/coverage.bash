d132b591ac869e8a8ff12e1ff361e106e881b45a
Tim Oram <dev@mitmaro.ca>
2020-09-23 12:40:30 -0230

    Enable coverage reporting with coveralls

--- scripts/coverage.bash
+++ scripts/coverage.bash
@@ -5,4 +5,4 @@ set -u
 set -o pipefail
 
 cargo +nightly install cargo-tarpaulin
-cargo +nightly tarpaulin --exclude-files=src/display/ncurses.rs --all-features --ignore-tests --line --verbose --out Html
+cargo +nightly tarpaulin --exclude-files=src/display/ncurses.rs --all-features --ignore-tests --line --verbose --out Html --out Lcov --output-dir coverage
