560c5943dc896231f99c581a3f4162075d9b6fa2
Tim Oram <dev@mitmaro.ca>
2020-11-11 21:45:46 -0330

    Improve and test show_commit::Delta
    Fix a bug where a context that contained an additional @ symbol would
    not properly parse and add tests to ensure correct parsing of the
    context.

--- src/show_commit/delta.rs
+++ src/show_commit/delta.rs
@@ -12,7 +12,7 @@ pub struct Delta {
 
 impl Delta {
 	pub(super) fn new(header: &str, old_start: u32, new_start: u32, old_lines: u32, new_lines: u32) -> Self {
-		let context = header.split('@').last().unwrap_or("").trim();
+		let context = header.splitn(3, '@').nth(2).unwrap_or("").trim();
 		Self {
 			old_start,
 			old_lines,
@@ -49,5 +49,54 @@ impl Delta {
 
 	pub(crate) const fn new_lines(&self) -> u32 {
 		self.new_lines
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::show_commit::diff_line::Origin;
+
+	#[test]
+	fn new_with_correctly_formatted_context() {
+		let delta = Delta::new("@ src/show_commit/delta.rs:56 @ impl Delta {", 10, 12, 3, 4);
+		assert_eq!(delta.context(), "impl Delta {");
+		assert_eq!(delta.old_start(), 10);
+		assert_eq!(delta.new_start(), 12);
+		assert_eq!(delta.old_lines(), 3);
+		assert_eq!(delta.new_lines(), 4);
+	}
+
+	#[test]
+	fn new_with_at_symbol_in_context() {
+		let delta = Delta::new("@ path:1 @ Context@", 10, 12, 3, 4);
+		assert_eq!(delta.context(), "Context@");
+		assert_eq!(delta.old_start(), 10);
+		assert_eq!(delta.new_start(), 12);
+		assert_eq!(delta.old_lines(), 3);
+		assert_eq!(delta.new_lines(), 4);
+	}
+
+	#[test]
+	fn new_with_incorrectly_formatted_context() {
+		let delta = Delta::new("@invalid", 10, 12, 3, 4);
+		assert_eq!(delta.context(), "");
+		assert_eq!(delta.old_start(), 10);
+		assert_eq!(delta.new_start(), 12);
+		assert_eq!(delta.old_lines(), 3);
+		assert_eq!(delta.new_lines(), 4);
+	}
+
+	#[test]
+	fn add_line() {
+		let mut delta = Delta::new("@ src/show_commit/delta.rs:56 @ impl Delta {", 10, 12, 3, 4);
+		delta.add_line(DiffLine::new(
+			Origin::Addition,
+			"this is a line",
+			Some(10),
+			Some(12),
+			false,
+		));
+		assert_eq!(delta.lines().len(), 1);
 	}
 }
