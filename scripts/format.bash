108d7935012ecd674221b36607217fce298d14b5
Tim Oram <dev@mitmaro.ca>
2020-11-14 16:44:45 -0330

    Use latest nightly for rustfmt
    The nightly version of Rust had to be pinned a while back due to a
    compatibility issue between Rust nightly and rustfmt. Undo this pin now
    that this issue is resolved.

--- scripts/format.bash
+++ scripts/format.bash
@@ -4,8 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-rust_version="nightly-2019-09-13"
+rustup update nightly
+rustup component add rustfmt --toolchain nightly
+cargo +nightly fmt --all -- --check
 
-rustup update "$rust_version"
-rustup component add rustfmt --toolchain "$rust_version"
-cargo +"$rust_version" fmt --all -- --check
