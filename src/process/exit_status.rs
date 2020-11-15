65295b3e15f931dc693924263a7df706e9adaa33
Tim Oram <dev@mitmaro.ca>
2020-11-15 19:59:43 -0330

    Add tests for ExitStatus enum

--- src/process/exit_status.rs
+++ src/process/exit_status.rs
@@ -18,3 +18,22 @@ impl ExitStatus {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use rstest::rstest;
+
+	#[rstest(
+		input,
+		expected,
+		case::config_error(ExitStatus::ConfigError, 1),
+		case::file_read_error(ExitStatus::FileReadError, 2),
+		case::file_write_error(ExitStatus::FileWriteError, 3),
+		case::good(ExitStatus::Good, 0),
+		case::state_error(ExitStatus::StateError, 4)
+	)]
+	fn to_code(input: ExitStatus, expected: i32) {
+		assert_eq!(ExitStatus::to_code(input), expected);
+	}
+}
