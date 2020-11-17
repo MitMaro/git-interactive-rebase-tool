6c0e6e3085467b04c1b14336ffd878322d59ee60
Tim Oram <dev@mitmaro.ca>
2020-11-17 10:08:10 -0330

    Add process::error tests
    Also fix bug caused by multi-line error rendering.

--- src/process/error.rs
+++ src/process/error.rs
@@ -51,12 +51,146 @@ impl Error {
 		self.view_data.reset();
 		self.view_data.set_show_title(true);
 		for cause in error.chain() {
-			self.view_data.push_line(ViewLine::from(format!("{:#}", cause)));
+			let error_text = format!("{:#}", cause);
+			for err in error_text.split('\n') {
+				self.view_data.push_line(ViewLine::from(err));
+			}
 		}
 		self.view_data
 			.push_trailing_line(ViewLine::from(LineSegment::new_with_color(
 				"Press any key to continue",
 				DisplayColor::IndicatorColor,
 			)));
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::assert_process_result;
+	use crate::assert_rendered_output;
+	use crate::process::testutil::{process_module_test, TestContext, ViewState};
+	use anyhow::anyhow;
+
+	#[test]
+	#[serial_test::serial]
+	fn simple_error() {
+		process_module_test(&[], ViewState::default(), &[], |test_context: TestContext<'_>| {
+			let mut module = Error::new();
+			module.set_error_message(&anyhow!("Test Error"));
+			let view_data = test_context.build_view_data(&mut module);
+			assert_rendered_output!(
+				view_data,
+				"{TITLE}",
+				"{BODY}",
+				"{Normal}Test Error",
+				"{TRAILING}",
+				"{IndicatorColor}Press any key to continue"
+			);
+		});
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn error_with_contest() {
+		process_module_test(&[], ViewState::default(), &[], |test_context: TestContext<'_>| {
+			let mut module = Error::new();
+			module.set_error_message(&anyhow!("Test Error").context("Context"));
+			let view_data = test_context.build_view_data(&mut module);
+			assert_rendered_output!(
+				view_data,
+				"{TITLE}",
+				"{BODY}",
+				"{Normal}Context",
+				"{Normal}Test Error",
+				"{TRAILING}",
+				"{IndicatorColor}Press any key to continue"
+			);
+		});
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn error_with_newlines() {
+		process_module_test(&[], ViewState::default(), &[], |test_context: TestContext<'_>| {
+			let mut module = Error::new();
+			module.set_error_message(&anyhow!("Test\nError").context("With\nContext"));
+			let view_data = test_context.build_view_data(&mut module);
+			assert_rendered_output!(
+				view_data,
+				"{TITLE}",
+				"{BODY}",
+				"{Normal}With",
+				"{Normal}Context",
+				"{Normal}Test",
+				"{Normal}Error",
+				"{TRAILING}",
+				"{IndicatorColor}Press any key to continue"
+			);
+		});
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn return_state() {
+		process_module_test(
+			&[],
+			ViewState::default(),
+			&[Input::Character('a')],
+			|mut test_context: TestContext<'_>| {
+				let mut module = Error::new();
+				module.activate(test_context.rebase_todo_file, State::ConfirmRebase);
+				module.set_error_message(&anyhow!("Test Error"));
+				assert_process_result!(
+					test_context.handle_input(&mut module),
+					input = Input::Character('a'),
+					state = State::ConfirmRebase
+				)
+			},
+		);
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn resize() {
+		process_module_test(
+			&[],
+			ViewState::default(),
+			&[Input::Resize],
+			|mut test_context: TestContext<'_>| {
+				let mut module = Error::new();
+				module.activate(test_context.rebase_todo_file, State::ConfirmRebase);
+				module.set_error_message(&anyhow!("Test Error"));
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::Resize)
+			},
+		);
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn scroll_events() {
+		process_module_test(
+			&[],
+			ViewState::default(),
+			&[
+				Input::ScrollLeft,
+				Input::ScrollRight,
+				Input::ScrollDown,
+				Input::ScrollUp,
+				Input::ScrollJumpDown,
+				Input::ScrollJumpUp,
+			],
+			|mut test_context: TestContext<'_>| {
+				let mut module = Error::new();
+				module.activate(test_context.rebase_todo_file, State::ConfirmRebase);
+				module.set_error_message(&anyhow!("Test Error"));
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollLeft);
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollRight);
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollDown);
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollUp);
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollJumpDown);
+				assert_process_result!(test_context.handle_input(&mut module), input = Input::ScrollJumpUp);
+			},
+		);
 	}
 }
