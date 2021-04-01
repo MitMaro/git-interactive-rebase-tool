d69dfd005345413c37e96a7011c4dc16bba44b69
Tim Oram <dev@mitmaro.ca>
2021-04-01 10:15:04 -0230

    Move name/version constants and delete file

--- src/constants.rs
+++
@@ -1,6 +0,0 @@
-pub const NAME: &str = "interactive-rebase-tool";
-
-#[cfg(not(feature = "dev"))]
-pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-#[cfg(feature = "dev")]
-pub const VERSION: &str = concat!(env!("CARGO_PKG_VERSION"), "-dev");
