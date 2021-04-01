c0c0144d169da0527234b5d70a638954ba1bae63
Tim Oram <dev@mitmaro.ca>
2021-04-01 10:14:07 -0230

    Move title consts to view module

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,3 @@
-pub const TITLE: &str = "Git Interactive Rebase Tool";
-pub const TITLE_LENGTH: usize = 27;
-pub const TITLE_SHORT: &str = "Git Rebase";
-pub const TITLE_SHORT_LENGTH: usize = 10;
-pub const TITLE_HELP_INDICATOR_LENGTH: usize = 6;
-
 pub const NAME: &str = "interactive-rebase-tool";
 
 #[cfg(not(feature = "dev"))]
