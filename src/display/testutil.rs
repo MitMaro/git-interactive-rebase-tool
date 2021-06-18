2b3f94268540ce8152b2d972b6bef0aa3e9160d9
Tim Oram <dev@mitmaro.ca>
2021-06-17 21:52:55 -0230

    Remove OUTPUT mutex from mocked Crossterm
    This removes the mutexed wrapped output in the mocked version of
    Crossterm allowing tests to not depend on single threaded testing.

--- src/display/testutil.rs
+++ src/display/testutil.rs
@@ -1,1 +1,6 @@
 pub use super::mockcrossterm::CrossTerm;
+use super::*;
+
+pub fn assert_output(display: &Display<CrossTerm>, expected: &[&str]) {
+	assert_eq!(display.tui.get_output().join(""), format!("{}\n", expected.join("\n")));
+}
