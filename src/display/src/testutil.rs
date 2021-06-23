126087ed90292fc710ab52ce80961ac54d08cfce
Tim Oram <dev@mitmaro.ca>
2021-06-23 09:31:02 -0230

    Inline module create functions

--- src/display/src/testutil.rs
+++ src/display/src/testutil.rs
@@ -1,6 +1,7 @@
 pub use super::mockcrossterm::CrossTerm;
 use super::*;
 
+#[inline]
 pub fn assert_output(display: &Display<CrossTerm>, expected: &[&str]) {
 	assert_eq!(display.tui.get_output().join(""), format!("{}\n", expected.join("\n")));
 }
