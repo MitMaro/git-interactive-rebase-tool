6e314d1bb2fb02a6c8452a0db96714b9c5900126
Tim Oram <dev@mitmaro.ca>
2021-06-29 09:39:22 -0230

    Add documentation to the view crate

--- src/view/src/action.rs
+++ src/view/src/action.rs
@@ -1,9 +1,15 @@
+/// An action to send to the thread handling updates to the view.
 #[derive(Debug, Copy, Clone)]
 #[allow(clippy::exhaustive_enums)]
 pub enum ViewAction {
+	/// Stop processing actions.
 	Stop,
+	/// Force a refresh of the view.
 	Refresh,
+	/// Render the latest `ViewData`.
 	Render,
+	/// Start processing actions.
 	Start,
+	/// End the thread and the processing of actions.
 	End,
 }
