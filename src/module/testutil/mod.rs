bd700d6dde79a8485780c8c5e416d157a997324a
Tim Oram <dev@mitmaro.ca>
2021-07-01 22:02:47 -0230

    Move handle_event_test to components module
    The handle_event_test utility function was only used in the components
    module, so move it to that module and remove the dependency on module.

--- src/module/testutil/mod.rs
+++ src/module/testutil/mod.rs
@@ -1,4 +1,3 @@
 mod assert_process_result;
-mod handle_event_test;
 
-pub use self::{assert_process_result::_assert_process_result, handle_event_test::handle_event_test};
+pub use self::assert_process_result::_assert_process_result;
