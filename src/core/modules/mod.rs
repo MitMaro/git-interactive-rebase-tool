16140ed58e5d40ebaca8779c26364a87b178ef89
Tim Oram <dev@mitmaro.ca>
2021-07-05 11:14:11 -0230

    Move external_editor module to core

--- src/core/modules/mod.rs
+++ src/core/modules/mod.rs
@@ -1,5 +1,11 @@
 mod confirm_abort;
 mod confirm_rebase;
 mod error;
+mod external_editor;
 
-pub use self::{confirm_abort::ConfirmAbort, confirm_rebase::ConfirmRebase, error::Error};
+pub use self::{
+	confirm_abort::ConfirmAbort,
+	confirm_rebase::ConfirmRebase,
+	error::Error,
+	external_editor::ExternalEditor,
+};
