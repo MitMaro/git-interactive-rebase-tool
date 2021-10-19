58910b4e4d2f06c685307c1839a49ff0a6ad4e04
Tim Oram <dev@mitmaro.ca>
2021-10-19 10:00:52 -0230

    Update config to use girt-git

--- src/config/src/key_bindings.rs
+++ src/config/src/key_bindings.rs
@@ -1,7 +1,10 @@
-use anyhow::Result;
-use git2::Config;
+use std::convert::TryFrom;
+
+use anyhow::{Error, Result};
+use git::Config;
 
 use super::utils::get_input;
+use crate::utils::map_single_ascii_to_lower;
 
 /// Represents the key binding configuration options.
 #[derive(Clone, Debug)]
@@ -75,15 +78,21 @@ pub struct KeyBindings {
 }
 
 impl KeyBindings {
-	/// Create a new key bindings from a Git Config reference.
-	pub(super) fn new(git_config: &Config) -> Result<Self> {
+	/// Create a new configuration with default values.
+	#[must_use]
+	#[inline]
+	pub fn new() -> Self {
+		Self::new_with_config(None).expect("Panic without git config instance") // should never error with None config
+	}
+
+	pub(super) fn new_with_config(git_config: Option<&Config>) -> Result<Self> {
 		let confirm_no = get_input(git_config, "interactive-rebase-tool.inputConfirmNo", "n")?
 			.iter()
-			.map(|s| s.as_str().to_lowercase())
+			.map(|s| map_single_ascii_to_lower(s))
 			.collect();
 		let confirm_yes = get_input(git_config, "interactive-rebase-tool.inputConfirmYes", "y")?
 			.iter()
-			.map(|s| s.as_str().to_lowercase())
+			.map(|s| map_single_ascii_to_lower(s))
 			.collect();
 		Ok(Self {
 			abort: get_input(git_config, "interactive-rebase-tool.inputAbort", "q")?,
@@ -120,5 +129,188 @@ impl KeyBindings {
 			toggle_visual_mode: get_input(git_config, "interactive-rebase-tool.inputToggleVisualMode", "v")?,
 			undo: get_input(git_config, "interactive-rebase-tool.inputUndo", "control+z")?,
 		})
+	}
+}
+
+impl Default for KeyBindings {
+	#[inline]
+	fn default() -> Self {
+		Self::new()
+	}
+}
+
+impl TryFrom<&Config> for KeyBindings {
+	type Error = Error;
+
+	#[inline]
+	fn try_from(config: &Config) -> core::result::Result<Self, Error> {
+		Self::new_with_config(Some(config))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use rstest::rstest;
+
+	use super::*;
+	use crate::testutils::{assert_error, invalid_utf, with_git_config};
+
+	pub(crate) fn with_keybindings<F>(lines: &[&str], callback: F)
+	where F: FnOnce(KeyBindings) {
+		with_git_config(lines, |config| {
+			let key_bindings = KeyBindings::new_with_config(Some(&config)).unwrap();
+			callback(key_bindings);
+		});
+	}
+
+	#[test]
+	fn new() {
+		let _config = KeyBindings::new();
+	}
+
+	#[test]
+	fn default() {
+		let _config = KeyBindings::default();
+	}
+
+	#[test]
+	fn try_from_git_config() {
+		with_git_config(&[], |git_config| {
+			assert!(KeyBindings::try_from(&git_config).is_ok());
+		});
+	}
+
+	#[test]
+	fn try_from_git_config_error() {
+		with_git_config(&["[interactive-rebase-tool]", "inputAbort = invalid"], |git_config| {
+			assert!(KeyBindings::try_from(&git_config).is_err());
+		});
+	}
+
+	#[rstest]
+	#[case::backspace("backspace", "Backspace")]
+	#[case::backtab("backtab", "BackTab")]
+	#[case::delete("delete", "Delete")]
+	#[case::down("down", "Down")]
+	#[case::end("end", "End")]
+	#[case::end("enter", "Enter")]
+	#[case::end("esc", "Esc")]
+	#[case::home("home", "Home")]
+	#[case::insert("insert", "Insert")]
+	#[case::left("left", "Left")]
+	#[case::pagedown("pagedown", "PageDown")]
+	#[case::pageup("pageup", "PageUp")]
+	#[case::right("right", "Right")]
+	#[case::tab("tab", "Tab")]
+	#[case::up("up", "Up")]
+	#[case::f1("f1", "F1")]
+	#[case::f255("f255", "F255")]
+	#[case::modifier_character_lowercase("Control+a", "Controla")]
+	#[case::modifier_character_uppercase("Control+A", "ControlA")]
+	#[case::modifier_character_number("Control+1", "Control1")]
+	#[case::modifier_character_special("Control++", "Control+")]
+	#[case::modifier_character("Control+a", "Controla")]
+	#[case::modifier_special("Control+End", "ControlEnd")]
+	#[case::modifier_function("Control+F32", "ControlF32")]
+	#[case::modifier_control_alt_shift_out_of_order_1("Alt+Shift+Control+End", "ShiftControlAltEnd")]
+	#[case::modifier_control_alt_shift_out_of_order_2("Shift+Control+Alt+End", "ShiftControlAltEnd")]
+	#[case::modifier_only_shift("Shift+End", "ShiftEnd")]
+	#[case::modifier_only_control("Control+End", "ControlEnd")]
+	#[case::modifier_only_control("a b c d", "a,b,c,d")]
+	#[case::modifier_only_control("Control+End Control+A", "ControlEnd,ControlA")]
+	fn value_parsing(#[case] binding: &str, #[case] expected: &str) {
+		with_keybindings(
+			&[
+				"[interactive-rebase-tool]",
+				format!("inputAbort = \"{}\"", binding).as_str(),
+			],
+			|key_bindings| {
+				assert_eq!(
+					key_bindings.abort,
+					expected.split(',').map(String::from).collect::<Vec<String>>()
+				);
+			},
+		);
+	}
+
+	#[rstest]
+	#[case::invalid_utf(
+		invalid_utf(),
+		"\"interactive-rebase-tool.inputAbort\" is not valid: configuration value is not valid utf8"
+	)]
+	#[case::multiple_characters(
+		"abcd",
+		"interactive-rebase-tool.inputAbort must contain only one character per binding"
+	)]
+	#[case::function_key_index(
+		"F256",
+		"interactive-rebase-tool.inputAbort must contain only one character per binding"
+	)]
+	#[case::multiple_bindings_one_invalid(
+		"f foo",
+		"interactive-rebase-tool.inputAbort must contain only one character per binding"
+	)]
+	fn value_parsing_invalid(#[case] binding: &str, #[case] expected_error: &str) {
+		with_git_config(
+			&[
+				"[interactive-rebase-tool]",
+				format!("inputAbort = {}", binding).as_str(),
+			],
+			|git_config| {
+				assert_error(KeyBindings::new_with_config(Some(&git_config)), expected_error);
+			},
+		);
+	}
+
+	#[rstest]
+	#[case::abort("inputAbort", "q", |bindings: KeyBindings| bindings.abort)]
+	#[case::action_break("inputActionBreak", "b", |bindings: KeyBindings| bindings.action_break)]
+	#[case::action_drop("inputActionDrop", "d", |bindings: KeyBindings| bindings.action_drop)]
+	#[case::action_edit("inputActionEdit", "e", |bindings: KeyBindings| bindings.action_edit)]
+	#[case::action_fixup("inputActionFixup", "f", |bindings: KeyBindings| bindings.action_fixup)]
+	#[case::action_pick("inputActionPick", "p", |bindings: KeyBindings| bindings.action_pick)]
+	#[case::action_reword("inputActionReword", "r", |bindings: KeyBindings| bindings.action_reword)]
+	#[case::action_squash("inputActionSquash", "s", |bindings: KeyBindings| bindings.action_squash)]
+	#[case::confirm_no("inputConfirmNo", "n", |bindings: KeyBindings| bindings.confirm_no)]
+	#[case::confirm_yes("inputConfirmYes", "y", |bindings: KeyBindings| bindings.confirm_yes)]
+	#[case::edit("inputEdit", "E", |bindings: KeyBindings| bindings.edit)]
+	#[case::force_abort("inputForceAbort", "Q", |bindings: KeyBindings| bindings.force_abort)]
+	#[case::force_rebase("inputForceRebase", "W", |bindings: KeyBindings| bindings.force_rebase)]
+	#[case::help("inputHelp", "?", |bindings: KeyBindings| bindings.help)]
+	#[case::insert_line("insertLine", "I", |bindings: KeyBindings| bindings.insert_line)]
+	#[case::move_down("inputMoveDown", "Down", |bindings: KeyBindings| bindings.move_down)]
+	#[case::move_down_step("inputMoveStepDown", "PageDown", |bindings: KeyBindings| bindings.move_down_step)]
+	#[case::move_end("inputMoveEnd", "End", |bindings: KeyBindings| bindings.move_end)]
+	#[case::move_home("inputMoveHome", "Home", |bindings: KeyBindings| bindings.move_home)]
+	#[case::move_left("inputMoveLeft", "Left", |bindings: KeyBindings| bindings.move_left)]
+	#[case::move_right("inputMoveRight", "Right", |bindings: KeyBindings| bindings.move_right)]
+	#[case::move_selection_down("inputMoveSelectionDown", "j", |bindings: KeyBindings| bindings.move_selection_down)]
+	#[case::move_selection_up("inputMoveSelectionUp", "k", |bindings: KeyBindings| bindings.move_selection_up)]
+	#[case::move_up("inputMoveUp", "Up", |bindings: KeyBindings| bindings.move_up)]
+	#[case::move_up_step("inputMoveStepUp", "PageUp", |bindings: KeyBindings| bindings.move_up_step)]
+	#[case::open_in_external_editor(
+		"inputOpenInExternalEditor",
+		"!",
+		|bindings: KeyBindings| bindings.open_in_external_editor)
+	]
+	#[case::rebase("inputRebase", "w", |bindings: KeyBindings| bindings.rebase)]
+	#[case::redo("inputRedo", "Controly", |bindings: KeyBindings| bindings.redo)]
+	#[case::remove_line("removeLine", "Delete", |bindings: KeyBindings| bindings.remove_line)]
+	#[case::show_commit("inputShowCommit", "c", |bindings: KeyBindings| bindings.show_commit)]
+	#[case::show_diff("inputShowDiff", "d", |bindings: KeyBindings| bindings.show_diff)]
+	#[case::toggle_visual_mode("inputToggleVisualMode", "v", |bindings: KeyBindings| bindings.toggle_visual_mode)]
+	#[case::undo("inputUndo", "Controlz", |bindings: KeyBindings| bindings.undo)]
+	pub(crate) fn test_binding<F: 'static>(#[case] config_name: &str, #[case] default: &str, #[case] access: F)
+	where F: Fn(KeyBindings) -> Vec<String> {
+		let default_keybindings = KeyBindings::new();
+		let binding = access(default_keybindings);
+		assert_eq!(binding, vec![String::from(default)]);
+
+		let config_value = format!("{} = \"f255\"", config_name);
+		with_git_config(&["[interactive-rebase-tool]", config_value.as_str()], |config| {
+			let key_bindings = KeyBindings::new_with_config(Some(&config)).unwrap();
+			let binding = access(key_bindings);
+			assert_eq!(binding, vec![String::from("F255")]);
+		});
 	}
 }
--- src/config/src/utils.rs
+++ src/config/src/utils.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use anyhow::{anyhow, Result};
-use git2::Config;
+use git::{Config, ErrorCode};
 
 use super::{
 	diff_ignore_whitespace_setting::DiffIgnoreWhitespaceSetting,
@@ -12,7 +12,7 @@ use super::{
 	Color,
 };
 
-pub(super) fn get_input(config: &Config, name: &str, default: &str) -> Result<Vec<String>> {
+pub(super) fn get_input(config: Option<&Config>, name: &str, default: &str) -> Result<Vec<String>> {
 	let mut values = vec![];
 	for mut value in get_string(config, name, default)?.split_whitespace().map(String::from) {
 		let mut modifiers = vec![];
@@ -56,8 +56,7 @@ pub(super) fn get_input(config: &Config, name: &str, default: &str) -> Result<Ve
 							value.to_uppercase()
 						}
 						else {
-							return Err(anyhow!("{} must contain only one character per binding", name)
-								.context("Error reading git config"));
+							return Err(anyhow!("{} must contain only one character per binding", name));
 						}
 					}
 					else {
@@ -70,41 +69,57 @@ pub(super) fn get_input(config: &Config, name: &str, default: &str) -> Result<Ve
 	Ok(values)
 }
 
-pub(super) fn get_string(config: &Config, name: &str, default: &str) -> Result<String> {
-	match config.get_string(name) {
+pub(super) fn get_string(config: Option<&Config>, name: &str, default: &str) -> Result<String> {
+	let cfg = match config {
+		None => return Ok(String::from(default)),
+		Some(c) => c,
+	};
+	match cfg.get_string(name) {
 		Ok(v) => Ok(v),
-		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(String::from(default)),
+		Err(ref e) if e.code() == ErrorCode::NotFound => Ok(String::from(default)),
 		Err(e) => Err(anyhow!(String::from(e.message()))),
 	}
 	.map_err(|e| e.context(anyhow!("\"{}\" is not valid", name)))
 }
 
-pub(super) fn get_bool(config: &Config, name: &str, default: bool) -> Result<bool> {
-	match config.get_bool(name) {
+pub(super) fn get_bool(config: Option<&Config>, name: &str, default: bool) -> Result<bool> {
+	let cfg = match config {
+		None => return Ok(default),
+		Some(c) => c,
+	};
+	match cfg.get_bool(name) {
 		Ok(v) => Ok(v),
-		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default),
+		Err(ref e) if e.code() == ErrorCode::NotFound => Ok(default),
 		Err(e) => Err(anyhow!(String::from(e.message()))),
 	}
 	.map_err(|e| e.context(anyhow!("\"{}\" is not valid", name)))
 }
 
 #[allow(clippy::map_err_ignore)]
-pub(super) fn get_unsigned_integer(config: &Config, name: &str, default: u32) -> Result<u32> {
-	match config.get_i32(name) {
+pub(super) fn get_unsigned_integer(config: Option<&Config>, name: &str, default: u32) -> Result<u32> {
+	let cfg = match config {
+		None => return Ok(default),
+		Some(c) => c,
+	};
+	match cfg.get_i32(name) {
 		Ok(v) => {
 			v.try_into()
 				.map_err(|_| anyhow!("\"{}\" is outside of valid range for an unsigned 32-bit integer", v))
 		},
-		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default),
+		Err(ref e) if e.code() == ErrorCode::NotFound => Ok(default),
 		Err(e) => Err(anyhow!(String::from(e.message()))),
 	}
 	.map_err(|e| e.context(anyhow!("\"{}\" is not valid", name)))
 }
 
-pub(super) fn get_color(config: &Config, name: &str, default_color: Color) -> Result<Color> {
-	match config.get_string(name) {
+pub(super) fn get_color(config: Option<&Config>, name: &str, default: Color) -> Result<Color> {
+	let cfg = match config {
+		None => return Ok(default),
+		Some(c) => c,
+	};
+	match cfg.get_string(name) {
 		Ok(v) => Color::try_from(v.to_lowercase().as_str()),
-		Err(ref e) if e.code() == git2::ErrorCode::NotFound => Ok(default_color),
+		Err(ref e) if e.code() == ErrorCode::NotFound => Ok(default),
 		Err(e) => Err(anyhow!(String::from(e.message()))),
 	}
 	.map_err(|e| e.context(anyhow!("\"{}\" is not valid", name)))
@@ -116,14 +131,7 @@ pub(super) fn editor_from_env() -> String {
 		.unwrap_or_else(|_| String::from("vi"))
 }
 
-pub(super) fn open_git_config() -> Result<Config> {
-	git2::Repository::open_from_env()
-		.map_err(|e| anyhow!(String::from(e.message())))?
-		.config()
-		.map_err(|e| anyhow!(String::from(e.message())))
-}
-
-pub(super) fn get_diff_show_whitespace(git_config: &Config) -> Result<DiffShowWhitespaceSetting> {
+pub(super) fn get_diff_show_whitespace(git_config: Option<&Config>) -> Result<DiffShowWhitespaceSetting> {
 	let diff_show_whitespace = get_string(git_config, "interactive-rebase-tool.diffShowWhitespace", "both")?;
 
 	match diff_show_whitespace.to_lowercase().as_str() {
@@ -142,7 +150,7 @@ pub(super) fn get_diff_show_whitespace(git_config: &Config) -> Result<DiffShowWh
 	}
 }
 
-pub(super) fn get_diff_ignore_whitespace(git_config: &Config) -> Result<DiffIgnoreWhitespaceSetting> {
+pub(super) fn get_diff_ignore_whitespace(git_config: Option<&Config>) -> Result<DiffIgnoreWhitespaceSetting> {
 	let diff_ignore_whitespace = get_string(git_config, "interactive-rebase-tool.diffIgnoreWhitespace", "none")?;
 
 	match diff_ignore_whitespace.to_lowercase().as_str() {
@@ -156,5 +164,14 @@ pub(super) fn get_diff_ignore_whitespace(git_config: &Config) -> Result<DiffIgno
 			)
 			.context("\"interactive-rebase-tool.diffIgnoreWhitespace\" is not valid"))
 		},
+	}
+}
+
+pub(super) fn map_single_ascii_to_lower(s: &str) -> String {
+	if s.is_ascii() && s.len() == 1 {
+		s.to_lowercase()
+	}
+	else {
+		String::from(s)
 	}
 }
