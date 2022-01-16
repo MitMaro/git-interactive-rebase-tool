67ec88e959e1d4acd6c7e5f3305c47d96b8d7b63
Tim Oram <dev@mitmaro.ca>
2022-01-16 11:16:23 -0330

    Fix error message for Windows tests

--- src/core/src/process/tests.rs
+++ src/core/src/process/tests.rs
@@ -433,7 +433,7 @@ fn handle_process_result_external_command_executable_not_found() {
 			"{BODY}",
 			format!("{{Normal}}Unable to run {}", command),
 			if cfg!(windows) {
-				"{Normal}The system cannot find the file specified. (os error 2)"
+				"{Normal}The system cannot find the path specified. (os error 3)"
 			}
 			else {
 				"{Normal}No such file or directory (os error 2)"
