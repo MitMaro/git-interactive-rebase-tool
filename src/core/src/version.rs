0f8f685084e1b244898850710eb035318fff5802
Tim Oram <dev@mitmaro.ca>
2022-05-23 22:15:19 -0230

    Add tests for core version

--- src/core/src/version.rs
+++ src/core/src/version.rs
@@ -20,3 +20,34 @@ fn build_version() -> String {
 pub(crate) fn run() -> Exit {
 	Exit::from(build_version())
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	#[serial_test::serial]
+	fn test_run() {
+		assert!(run()
+			.get_message()
+			.as_ref()
+			.unwrap()
+			.starts_with("interactive-rebase-tool"))
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn build_version_default() {
+		let version = build_version();
+		assert!(version.starts_with("interactive-rebase-tool"));
+	}
+
+	#[test]
+	#[serial_test::serial]
+	fn build_version_with_env() {
+		let version = build_version();
+		let expected_meta = format!("({} {})", env!("GIRT_BUILD_GIT_HASH"), env!("GIRT_BUILD_DATE"));
+		assert!(version.starts_with("interactive-rebase-tool"));
+		assert!(version.ends_with(expected_meta.as_str()));
+	}
+}
