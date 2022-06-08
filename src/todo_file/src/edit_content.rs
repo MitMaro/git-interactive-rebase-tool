403434dd04b939a2ccd866efddebde22c5b9bf5b
Tim Oram <dev@mitmaro.ca>
2022-06-08 10:27:52 -0230

    Cleanup and refactor EditContext
    EditContext was returning references to `Option`s when it should instead
    return `Option`s containing references. In the case of `Action`, it
    implements `Copy` so instead can be returned as a simple copy.
    
    The tests have been updated to take advantage of of rust-claim.

--- src/todo_file/src/edit_content.rs
+++ src/todo_file/src/edit_content.rs
@@ -30,54 +30,56 @@ impl EditContext {
 	#[must_use]
 	#[inline]
 	pub fn content(mut self, content: &str) -> Self {
-		self.content = Some(content.to_owned());
+		self.content = Some(String::from(content));
 		self
 	}
 
 	/// Get the action.
 	#[must_use]
 	#[inline]
-	pub const fn get_action(&self) -> &Option<Action> {
-		&self.action
+	pub const fn get_action(&self) -> Option<Action> {
+		self.action
 	}
 
 	/// Get the content.
 	#[must_use]
 	#[inline]
-	pub const fn get_content(&self) -> &Option<String> {
-		&self.content
+	pub fn get_content(&self) -> Option<&str> {
+		self.content.as_deref()
 	}
 }
 
 #[cfg(test)]
 mod tests {
+	use claim::{assert_none, assert_some_eq};
+
 	use super::*;
 
 	#[test]
 	fn empty() {
 		let edit_context = EditContext::new();
-		assert_eq!(edit_context.get_action(), &None);
-		assert_eq!(edit_context.get_content(), &None);
+		assert_none!(edit_context.get_action());
+		assert_none!(edit_context.get_content());
 	}
 
 	#[test]
 	fn with_action() {
 		let edit_context = EditContext::new().action(Action::Break);
-		assert_eq!(edit_context.get_action(), &Some(Action::Break));
-		assert_eq!(edit_context.get_content(), &None);
+		assert_some_eq!(edit_context.get_action(), Action::Break);
+		assert_none!(edit_context.get_content(),);
 	}
 
 	#[test]
 	fn with_content() {
 		let edit_context = EditContext::new().content("test content");
-		assert_eq!(edit_context.get_action(), &None);
-		assert_eq!(edit_context.get_content(), &Some(String::from("test content")));
+		assert_none!(edit_context.get_action());
+		assert_some_eq!(edit_context.get_content(), "test content");
 	}
 
 	#[test]
 	fn with_content_and_action() {
 		let edit_context = EditContext::new().action(Action::Edit).content("test content");
-		assert_eq!(edit_context.get_action(), &Some(Action::Edit));
-		assert_eq!(edit_context.get_content(), &Some(String::from("test content")));
+		assert_some_eq!(edit_context.get_action(), Action::Edit);
+		assert_some_eq!(edit_context.get_content(), "test content");
 	}
 }
