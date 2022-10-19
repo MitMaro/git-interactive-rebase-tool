908f11bd58ed806c6c86756be57c2cbf1015ca0e
Tim Oram <dev@mitmaro.ca>
2022-10-19 10:03:06 -0230

    Refactor list module help

--- src/core/src/modules/list/utils.rs
+++ src/core/src/modules/list/utils.rs
@@ -5,182 +5,175 @@ use display::DisplayColor;
 use todo_file::{Action, Line};
 use view::LineSegment;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum HelpLinesSelector {
+	Normal,
+	Visual,
+	Common,
+}
+
+fn build_help_lines(key_bindings: &KeyBindings, selector: HelpLinesSelector) -> Vec<(Vec<String>, String)> {
+	let lines = vec![
+		(&key_bindings.move_up, "Move selection up", HelpLinesSelector::Common),
+		(
+			&key_bindings.move_down,
+			"Move selection down",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_up_step,
+			"Move selection up half a page",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_down_step,
+			"Move selection down half a page",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_home,
+			"Move selection to top of the list",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_end,
+			"Move selection to end of the list",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_left,
+			"Scroll content to the left",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_right,
+			"Scroll content to the right",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.abort,
+			"Abort interactive rebase",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.force_abort,
+			"Immediately abort interactive rebase",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.rebase,
+			"Write interactive rebase file",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.force_rebase,
+			"Immediately write interactive rebase file",
+			HelpLinesSelector::Common,
+		),
+		(&key_bindings.help, "Show help", HelpLinesSelector::Common),
+		(
+			&key_bindings.move_selection_down,
+			"Move selected lines down",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.move_selection_up,
+			"Move selected lines up",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.show_commit,
+			"Show commit information",
+			HelpLinesSelector::Normal,
+		),
+		(
+			&key_bindings.action_break,
+			"Toggle break action",
+			HelpLinesSelector::Normal,
+		),
+		(
+			&key_bindings.action_pick,
+			"Set selected commits to be picked",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.action_reword,
+			"Set selected commits to be reworded",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.action_edit,
+			"Set selected commits to be edited",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.action_squash,
+			"Set selected commits to be squashed",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.action_fixup,
+			"Set selected commits to be fixed-up",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.action_drop,
+			"Set selected commits to be dropped",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.edit,
+			"Edit an exec, label, reset or merge action's content",
+			HelpLinesSelector::Normal,
+		),
+		(
+			&key_bindings.insert_line,
+			"Insert a new line",
+			HelpLinesSelector::Normal,
+		),
+		(
+			&key_bindings.remove_line,
+			"Completely remove the selected lines",
+			HelpLinesSelector::Common,
+		),
+		(&key_bindings.undo, "Undo the last change", HelpLinesSelector::Common),
+		(
+			&key_bindings.redo,
+			"Redo the previous undone change",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.open_in_external_editor,
+			"Open the todo file in the default editor",
+			HelpLinesSelector::Common,
+		),
+		(
+			&key_bindings.toggle_visual_mode,
+			"Enter visual mode",
+			HelpLinesSelector::Normal,
+		),
+		(
+			&key_bindings.toggle_visual_mode,
+			"Exit visual mode",
+			HelpLinesSelector::Visual,
+		),
+	];
+
+	lines
+		.iter()
+		.filter_map(|&(binding, help, line_selector)| {
+			let selected = line_selector == selector || line_selector == HelpLinesSelector::Common;
+			selected.then(|| ((*binding).clone(), String::from(help)))
+		})
+		.collect()
+}
+
 pub(super) fn get_list_normal_mode_help_lines(key_bindings: &KeyBindings) -> Vec<(Vec<String>, String)> {
-	vec![
-		(key_bindings.move_up.clone(), String::from("Move selection up")),
-		(key_bindings.move_down.clone(), String::from("Move selection down")),
-		(
-			key_bindings.move_up_step.clone(),
-			String::from("Move selection up 5 lines"),
-		),
-		(
-			key_bindings.move_down_step.clone(),
-			String::from("Move selection down 5 lines"),
-		),
-		(
-			key_bindings.move_home.clone(),
-			String::from("Move selection to top of the list"),
-		),
-		(
-			key_bindings.move_end.clone(),
-			String::from("Move selection to end of the list"),
-		),
-		(
-			key_bindings.move_left.clone(),
-			String::from("Scroll content to the left"),
-		),
-		(
-			key_bindings.move_right.clone(),
-			String::from("Scroll content to the right"),
-		),
-		(key_bindings.abort.clone(), String::from("Abort interactive rebase")),
-		(
-			key_bindings.force_abort.clone(),
-			String::from("Immediately abort interactive rebase"),
-		),
-		(
-			key_bindings.rebase.clone(),
-			String::from("Write interactive rebase file"),
-		),
-		(
-			key_bindings.force_rebase.clone(),
-			String::from("Immediately write interactive rebase file"),
-		),
-		(
-			key_bindings.toggle_visual_mode.clone(),
-			String::from("Enter visual mode"),
-		),
-		(key_bindings.help.clone(), String::from("Show help")),
-		(
-			key_bindings.show_commit.clone(),
-			String::from("Show commit information"),
-		),
-		(
-			key_bindings.move_selection_down.clone(),
-			String::from("Move selected commit down"),
-		),
-		(
-			key_bindings.move_selection_up.clone(),
-			String::from("Move selected commit up"),
-		),
-		(key_bindings.action_break.clone(), String::from("Toggle break action")),
-		(
-			key_bindings.action_pick.clone(),
-			String::from("Set selected commit to be picked"),
-		),
-		(
-			key_bindings.action_reword.clone(),
-			String::from("Set selected commit to be reworded"),
-		),
-		(
-			key_bindings.action_edit.clone(),
-			String::from("Set selected commit to be edited"),
-		),
-		(
-			key_bindings.action_squash.clone(),
-			String::from("Set selected commit to be squashed"),
-		),
-		(
-			key_bindings.action_fixup.clone(),
-			String::from("Set selected commit to be fixed-up"),
-		),
-		(
-			key_bindings.action_drop.clone(),
-			String::from("Set selected commit to be dropped"),
-		),
-		(key_bindings.edit.clone(), String::from("Edit an exec action's command")),
-		(key_bindings.insert_line.clone(), String::from("Insert a new line")),
-		(
-			key_bindings.remove_line.clone(),
-			String::from("Completely remove the selected line"),
-		),
-		(key_bindings.undo.clone(), String::from("Undo the last change")),
-		(
-			key_bindings.redo.clone(),
-			String::from("Redo the previous undone change"),
-		),
-		(
-			key_bindings.open_in_external_editor.clone(),
-			String::from("Open the todo file in the default editor"),
-		),
-	]
+	build_help_lines(key_bindings, HelpLinesSelector::Normal)
 }
 
 pub(super) fn get_list_visual_mode_help_lines(key_bindings: &KeyBindings) -> Vec<(Vec<String>, String)> {
-	vec![
-		(key_bindings.move_up.clone(), String::from("Move selection up")),
-		(key_bindings.move_down.clone(), String::from("Move selection down")),
-		(
-			key_bindings.move_up_step.clone(),
-			String::from("Move selection up 5 lines"),
-		),
-		(
-			key_bindings.move_down_step.clone(),
-			String::from("Move selection down 5 lines"),
-		),
-		(
-			key_bindings.move_home.clone(),
-			String::from("Move selection to top of the list"),
-		),
-		(
-			key_bindings.move_end.clone(),
-			String::from("Move selection to end of the list"),
-		),
-		(
-			key_bindings.move_left.clone(),
-			String::from("Scroll content to the left"),
-		),
-		(
-			key_bindings.move_right.clone(),
-			String::from("Scroll content to the right"),
-		),
-		(key_bindings.help.clone(), String::from("Show help")),
-		(
-			key_bindings.move_selection_down.clone(),
-			String::from("Move selected commits down"),
-		),
-		(
-			key_bindings.move_selection_up.clone(),
-			String::from("Move selected commits up"),
-		),
-		(
-			key_bindings.action_pick.clone(),
-			String::from("Set selected commits to be picked"),
-		),
-		(
-			key_bindings.action_reword.clone(),
-			String::from("Set selected commits to be reworded"),
-		),
-		(
-			key_bindings.action_edit.clone(),
-			String::from("Set selected commits to be edited"),
-		),
-		(
-			key_bindings.action_squash.clone(),
-			String::from("Set selected commits to be squashed"),
-		),
-		(
-			key_bindings.action_fixup.clone(),
-			String::from("Set selected commits to be fixed-up"),
-		),
-		(
-			key_bindings.action_drop.clone(),
-			String::from("Set selected commits to be dropped"),
-		),
-		(
-			key_bindings.remove_line.clone(),
-			String::from("Completely remove the selected lines"),
-		),
-		(key_bindings.undo.clone(), String::from("Undo the last change")),
-		(
-			key_bindings.redo.clone(),
-			String::from("Redo the previous undone change"),
-		),
-		(
-			key_bindings.toggle_visual_mode.clone(),
-			String::from("Exit visual mode"),
-		),
-	]
+	build_help_lines(key_bindings, HelpLinesSelector::Visual)
 }
 
 const fn get_action_color(action: Action) -> DisplayColor {
