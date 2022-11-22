eaba86847a1f04fab2011c76fafe45ce2becac6d
Tim Oram <dev@mitmaro.ca>
2022-11-22 09:37:30 -0330

    Add flag support to todo_file Line

--- src/todo_file/src/edit_content.rs
+++ src/todo_file/src/edit_content.rs
@@ -5,6 +5,7 @@ use super::action::Action;
 pub struct EditContext {
 	action: Option<Action>,
 	content: Option<String>,
+	option: Option<String>,
 }
 
 impl EditContext {
@@ -15,6 +16,7 @@ impl EditContext {
 		Self {
 			action: None,
 			content: None,
+			option: None,
 		}
 	}
 
@@ -34,6 +36,14 @@ impl EditContext {
 		self
 	}
 
+	/// Set the option.
+	#[must_use]
+	#[inline]
+	pub fn option(mut self, option: &str) -> Self {
+		self.option = Some(String::from(option));
+		self
+	}
+
 	/// Get the action.
 	#[must_use]
 	#[inline]
@@ -46,6 +56,13 @@ impl EditContext {
 	#[inline]
 	pub fn get_content(&self) -> Option<&str> {
 		self.content.as_deref()
+	}
+
+	/// Get the option.
+	#[must_use]
+	#[inline]
+	pub fn get_option(&self) -> Option<&str> {
+		self.option.as_deref()
 	}
 }
 
@@ -60,13 +77,15 @@ mod tests {
 		let edit_context = EditContext::new();
 		assert_none!(edit_context.get_action());
 		assert_none!(edit_context.get_content());
+		assert_none!(edit_context.get_option());
 	}
 
 	#[test]
 	fn with_action() {
 		let edit_context = EditContext::new().action(Action::Break);
 		assert_some_eq!(edit_context.get_action(), Action::Break);
-		assert_none!(edit_context.get_content(),);
+		assert_none!(edit_context.get_content());
+		assert_none!(edit_context.get_option());
 	}
 
 	#[test]
@@ -74,12 +93,25 @@ mod tests {
 		let edit_context = EditContext::new().content("test content");
 		assert_none!(edit_context.get_action());
 		assert_some_eq!(edit_context.get_content(), "test content");
+		assert_none!(edit_context.get_option());
 	}
 
 	#[test]
-	fn with_content_and_action() {
-		let edit_context = EditContext::new().action(Action::Edit).content("test content");
+	fn with_option() {
+		let edit_context = EditContext::new().option("-C");
+		assert_none!(edit_context.get_action());
+		assert_none!(edit_context.get_content());
+		assert_some_eq!(edit_context.get_option(), "-C");
+	}
+
+	#[test]
+	fn with_all() {
+		let edit_context = EditContext::new()
+			.action(Action::Edit)
+			.content("test content")
+			.option("-C");
 		assert_some_eq!(edit_context.get_action(), Action::Edit);
 		assert_some_eq!(edit_context.get_content(), "test content");
+		assert_some_eq!(edit_context.get_option(), "-C");
 	}
 }
---
+++ src/todo_file/src/line_parser.rs
@@ -0,0 +1,191 @@
+use crate::errors::ParseError;
+
+pub(crate) struct LineParser<'line> {
+	input: &'line str,
+	index: usize,
+}
+
+impl<'line> LineParser<'line> {
+	pub(crate) fn new(input: &'line str) -> Self {
+		let mut index = 0;
+		while input.get(index..=index).unwrap_or("") == " " {
+			index += 1;
+		}
+		Self { input, index }
+	}
+
+	pub(crate) const fn has_more(&self) -> bool {
+		self.index < self.input.len()
+	}
+
+	#[allow(clippy::unwrap_in_result)]
+	fn scan(&mut self) -> Result<(&'line str, usize), ParseError> {
+		let mut new_index = self.index;
+		if !self.has_more() {
+			return Err(self.parse_error());
+		}
+
+		loop {
+			if self.input.get(new_index..=new_index).unwrap_or(" ") == " " {
+				let slice = self.input.get(self.index..new_index).unwrap();
+				// skip whitespace
+				new_index += 1;
+				while self.input.get(new_index..=new_index).unwrap_or("") == " " {
+					new_index += 1;
+				}
+				return Ok((slice, new_index));
+			}
+			new_index += 1;
+		}
+	}
+
+	pub(crate) fn next(&mut self) -> Result<&'line str, ParseError> {
+		let (slice, new_index) = self.scan()?;
+		self.index = new_index;
+		Ok(slice)
+	}
+
+	pub(crate) fn take_remaining(self) -> &'line str {
+		self.input.get(self.index..self.input.len()).unwrap_or("")
+	}
+
+	pub(crate) fn parse_error(&self) -> ParseError {
+		ParseError::InvalidLine(String::from(self.input))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use testutils::assert_err_eq;
+
+	use super::*;
+
+	fn collect_all_tokens<'parser>(parser: &'parser mut LineParser<'parser>) -> Vec<&'parser str> {
+		let mut tokens = vec![];
+		while let Ok(t) = parser.next() {
+			tokens.push(t);
+		}
+		tokens
+	}
+
+	#[test]
+	fn has_more_new() {
+		let parser = LineParser::new("foo");
+		assert!(parser.has_more());
+	}
+
+	#[test]
+	fn has_more_after_next() {
+		let mut parser = LineParser::new("foo");
+		let _ = parser.next();
+		assert!(!parser.has_more());
+	}
+
+	#[test]
+	fn has_more_after_next_with_trailing_spaces() {
+		let mut parser = LineParser::new("foo ");
+		let _ = parser.next();
+		assert!(!parser.has_more());
+	}
+
+	#[test]
+	fn next_single_token() {
+		let mut parser = LineParser::new("foo");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo"]);
+	}
+
+	#[test]
+	fn next_single_token_leading_space() {
+		let mut parser = LineParser::new(" foo");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo"]);
+	}
+
+	#[test]
+	fn next_single_token_trailing_space() {
+		let mut parser = LineParser::new("foo ");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo"]);
+	}
+
+	#[test]
+	fn next_multiple_tokens() {
+		let mut parser = LineParser::new("foo bar foobar");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo", "bar", "foobar"]);
+	}
+
+	#[test]
+	fn next_tokens_with_multiple_spaces_between() {
+		let mut parser = LineParser::new("foo   bar  foobar");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo", "bar", "foobar"]);
+	}
+
+	#[test]
+	fn next_tokens_trailing_spaces() {
+		let mut parser = LineParser::new("foo bar foobar ");
+		assert_eq!(collect_all_tokens(&mut parser), vec!["foo", "bar", "foobar"]);
+	}
+
+	#[test]
+	fn next_empty_str() {
+		let mut parser = LineParser::new("");
+		assert_err_eq!(parser.next(), ParseError::InvalidLine(String::new()));
+	}
+
+	#[test]
+	fn next_space_only_str() {
+		let mut parser = LineParser::new(" ");
+		assert_err_eq!(parser.next(), ParseError::InvalidLine(String::from(" ")));
+	}
+
+	#[test]
+	fn next_end_of_tokens() {
+		let mut parser = LineParser::new("foo");
+		let _ = parser.next();
+		assert_err_eq!(parser.next(), ParseError::InvalidLine(String::from("foo")));
+	}
+
+	#[test]
+	fn next_end_of_tokens_with_trailing_space() {
+		let mut parser = LineParser::new("foo ");
+		let _ = parser.next();
+		assert_err_eq!(parser.next(), ParseError::InvalidLine(String::from("foo ")));
+	}
+
+	#[test]
+	fn take_remaining_new() {
+		let parser = LineParser::new("foo");
+		assert_eq!(parser.take_remaining(), "foo");
+	}
+
+	#[test]
+	fn take_remaining_empty_str() {
+		let parser = LineParser::new("");
+		assert_eq!(parser.take_remaining(), "");
+	}
+
+	#[test]
+	fn take_remaining_space_str() {
+		let parser = LineParser::new(" ");
+		assert_eq!(parser.take_remaining(), "");
+	}
+
+	#[test]
+	fn take_remaining_after_next() {
+		let mut parser = LineParser::new("foo bar");
+		let _ = parser.next();
+		assert_eq!(parser.take_remaining(), "bar");
+	}
+
+	#[test]
+	fn take_remaining_end_of_tokens() {
+		let mut parser = LineParser::new("foo");
+		let _ = parser.next();
+		assert_eq!(parser.take_remaining(), "");
+	}
+
+	#[test]
+	fn take_remaining_end_of_tokens_trailing_space() {
+		let mut parser = LineParser::new("foo ");
+		let _ = parser.next();
+		assert_eq!(parser.take_remaining(), "");
+	}
+}
