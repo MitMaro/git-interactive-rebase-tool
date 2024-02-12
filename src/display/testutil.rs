222f09c87a85ea342bb7aa58c764159735fbe21e
Tim Oram <dev@mitmaro.ca>
2024-02-11 21:19:48 -0330

    Remove assert_output test util

--- src/display/testutil.rs
+++ src/display/testutil.rs
@@ -8,14 +8,3 @@ pub(crate) use self::{
 	mockcrossterm::CrossTerm,
 	state::State,
 };
-use crate::display::Display;
-
-/// Assert the the content of the Display is an expected value.
-///
-/// # Panics
-///
-/// Will panic is the expected output does not match the rendered output.
-#[allow(clippy::missing_assert_message)] // not sure why this is triggering
-pub(crate) fn assert_output(display: &Display<CrossTerm>, expected: &[&str]) {
-	assert_eq!(display.tui.get_output().join(""), format!("{}\n", expected.join("\n")));
-}
--- src/display/testutil/mockcrossterm.rs
+++ src/display/testutil/mockcrossterm.rs
@@ -1,4 +1,7 @@
+use std::{ops::Deref, sync::Arc};
+
 use ::crossterm::style::{Attribute, Attributes, Color, Colors};
+use parking_lot::RwLock;
 
 use crate::display::{
 	testutil::{MockableTui, State},
@@ -7,9 +10,8 @@ use crate::display::{
 	Size,
 };
 
-/// A mocked version of `CrossTerm`, useful for testing.
 #[derive(Debug)]
-pub(crate) struct CrossTerm {
+struct CrossTermInternalState {
 	attributes: Attributes,
 	color_mode: ColorMode,
 	colors: Colors,
@@ -20,87 +22,95 @@ pub(crate) struct CrossTerm {
 	state: State,
 }
 
+/// A mocked version of `CrossTerm`, useful for testing.
+#[derive(Debug)]
+pub(crate) struct CrossTerm {
+	state: Arc<RwLock<CrossTermInternalState>>,
+}
+
 impl MockableTui for CrossTerm {
 	fn get_color_mode(&self) -> ColorMode {
-		self.color_mode
+		self.state.read().color_mode
 	}
 
 	fn reset(&mut self) -> Result<(), DisplayError> {
-		self.attributes = Attributes::from(Attribute::Reset);
-		self.colors = Colors::new(Color::Reset, Color::Reset);
-		self.output.clear();
-		self.state = State::Normal;
+		let mut state = self.state.write();
+		state.attributes = Attributes::from(Attribute::Reset);
+		state.colors = Colors::new(Color::Reset, Color::Reset);
+		state.output.clear();
+		state.state = State::Normal;
 		Ok(())
 	}
 
 	fn flush(&mut self) -> Result<(), DisplayError> {
-		self.dirty = false;
+		self.state.write().dirty = false;
 		Ok(())
 	}
 
 	fn print(&mut self, s: &str) -> Result<(), DisplayError> {
-		self.output.push(String::from(s));
+		self.state.write().output.push(String::from(s));
 		Ok(())
 	}
 
 	fn set_color(&mut self, colors: Colors) -> Result<(), DisplayError> {
-		self.colors = colors;
+		self.state.write().colors = colors;
 		Ok(())
 	}
 
 	fn set_dim(&mut self, dim: bool) -> Result<(), DisplayError> {
 		if dim {
-			self.attributes.set(Attribute::Dim);
+			self.state.write().attributes.set(Attribute::Dim);
 		}
 		else {
-			self.attributes.set(Attribute::NormalIntensity);
+			self.state.write().attributes.set(Attribute::NormalIntensity);
 		}
 		Ok(())
 	}
 
 	fn set_underline(&mut self, dim: bool) -> Result<(), DisplayError> {
 		if dim {
-			self.attributes.set(Attribute::Underlined);
+			self.state.write().attributes.set(Attribute::Underlined);
 		}
 		else {
-			self.attributes.set(Attribute::NoUnderline);
+			self.state.write().attributes.set(Attribute::NoUnderline);
 		}
 		Ok(())
 	}
 
 	fn set_reverse(&mut self, dim: bool) -> Result<(), DisplayError> {
 		if dim {
-			self.attributes.set(Attribute::Reverse);
+			self.state.write().attributes.set(Attribute::Reverse);
 		}
 		else {
-			self.attributes.set(Attribute::NoReverse);
+			self.state.write().attributes.set(Attribute::NoReverse);
 		}
 		Ok(())
 	}
 
 	fn get_size(&self) -> Size {
-		self.size
+		self.state.read().size
 	}
 
 	fn move_to_column(&mut self, x: u16) -> Result<(), DisplayError> {
-		self.position.0 = x;
+		self.state.write().position.0 = x;
 		Ok(())
 	}
 
 	fn move_next_line(&mut self) -> Result<(), DisplayError> {
-		self.output.push(String::from("\n"));
-		self.position.0 = 0;
-		self.position.1 += 1;
+		let mut state = self.state.write();
+		state.output.push(String::from("\n"));
+		state.position.0 = 0;
+		state.position.1 += 1;
 		Ok(())
 	}
 
 	fn start(&mut self) -> Result<(), DisplayError> {
-		self.state = State::Normal;
+		self.state.write().state = State::Normal;
 		Ok(())
 	}
 
 	fn end(&mut self) -> Result<(), DisplayError> {
-		self.state = State::Ended;
+		self.state.write().state = State::Ended;
 		Ok(())
 	}
 }
@@ -110,67 +120,77 @@ impl CrossTerm {
 	#[must_use]
 	pub(crate) fn new() -> Self {
 		Self {
-			attributes: Attributes::from(Attribute::Reset),
-			color_mode: ColorMode::FourBit,
-			colors: Colors::new(Color::Reset, Color::Reset),
-			dirty: true,
-			output: vec![],
-			position: (0, 0),
-			size: Size::new(10, 10),
-			state: State::New,
+			state: Arc::new(RwLock::new(CrossTermInternalState {
+				attributes: Attributes::from(Attribute::Reset),
+				color_mode: ColorMode::FourBit,
+				colors: Colors::new(Color::Reset, Color::Reset),
+				dirty: true,
+				output: vec![],
+				position: (0, 0),
+				size: Size::new(10, 10),
+				state: State::New,
+			})),
 		}
 	}
 
 	/// Get a representation of the rendered output.
 	#[must_use]
-	pub(crate) const fn get_output(&self) -> &Vec<String> {
-		&self.output
+	pub(crate) fn get_output(&self) -> Vec<String> {
+		self.state.read().output.clone()
 	}
 
 	/// Get the current state.
 	#[must_use]
-	pub(crate) const fn get_state(&self) -> State {
-		self.state
+	pub(crate) fn get_state(&self) -> State {
+		self.state.read().state
 	}
 
 	/// Are colors enabled.
 	#[must_use]
 	pub(crate) fn is_colors_enabled(&self, colors: Colors) -> bool {
-		self.colors == colors
+		self.state.read().colors == colors
 	}
 
 	/// Does the current style attributes contained dimmed.
 	#[must_use]
-	pub(crate) const fn is_dimmed(&self) -> bool {
-		self.attributes.has(Attribute::Dim)
+	pub(crate) fn is_dimmed(&self) -> bool {
+		self.state.read().attributes.has(Attribute::Dim)
 	}
 
 	/// Does the current style attributes contained reverse.
 	#[must_use]
-	pub(crate) const fn is_reverse(&self) -> bool {
-		self.attributes.has(Attribute::Reverse)
+	pub(crate) fn is_reverse(&self) -> bool {
+		self.state.read().attributes.has(Attribute::Reverse)
 	}
 
 	/// Does the current style attributes contained underlined.
 	#[must_use]
-	pub(crate) const fn is_underline(&self) -> bool {
-		self.attributes.has(Attribute::Underlined)
+	pub(crate) fn is_underline(&self) -> bool {
+		self.state.read().attributes.has(Attribute::Underlined)
 	}
 
 	/// Update the size.
 	pub(crate) fn set_size(&mut self, size: Size) {
-		self.size = size;
+		self.state.write().size = size;
 	}
 
 	/// Get the current cursor position.
 	#[must_use]
-	pub(crate) const fn get_position(&self) -> (u16, u16) {
-		self.position
+	pub(crate) fn get_position(&self) -> (u16, u16) {
+		self.state.read().position
 	}
 
 	/// Has the output been flushed.
 	#[must_use]
-	pub(crate) const fn is_dirty(&self) -> bool {
-		self.dirty
+	pub(crate) fn is_dirty(&self) -> bool {
+		self.state.read().dirty
+	}
+}
+
+impl Clone for CrossTerm {
+	fn clone(&self) -> Self {
+		CrossTerm {
+			state: Arc::clone(&self.state),
+		}
 	}
 }
