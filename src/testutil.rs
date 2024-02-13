5e1a39ae5b83b0c1344f5426b364484a1917b5b1
Tim Oram <dev@mitmaro.ca>
2024-02-12 22:35:01 -0330

    Move assert_results to test helpers

--- src/testutil.rs
+++ src/testutil.rs
@@ -1,4 +1,3 @@
-mod assert_results;
 mod mocked_searchable;
 mod module_test;
 mod process_test;
@@ -8,7 +7,6 @@ mod test_module_provider;
 mod with_search;
 
 pub(crate) use self::{
-	assert_results::_assert_results,
 	mocked_searchable::MockedSearchable,
 	module_test::module_test,
 	process_test::{process_test, TestContext as ProcessTestContext},
