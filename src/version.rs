3282108f014f0bf98fbb254aa7d4e7a934c25b7a
Benjamin Gilbert <bgilbert@backtick.net>
2024-06-30 18:26:21 -0500

    Fix failure building build_version_with_env test outside of Git
    If we're building tests from an unpacked crate instead of a Git repo, we
    won't have a commit hash.

--- src/version.rs
+++ src/version.rs
@@ -47,8 +47,23 @@ mod tests {
 	#[test]
 	#[serial_test::serial]
 	fn build_version_with_env() {
+		let maybe_git_hash = option_env!("GIRT_BUILD_GIT_HASH");
+		assert_eq!(
+			std::process::Command::new("git")
+				.args(["rev-parse", "--is-inside-work-tree"])
+				.output()
+				.map(|out| out.status.success())
+				.unwrap_or(false),
+			maybe_git_hash.is_some()
+		);
+
 		let version = build_version();
-		let expected_meta = format!("({} {})", env!("GIRT_BUILD_GIT_HASH"), env!("GIRT_BUILD_DATE"));
+		let expected_meta = if let Some(git_hash) = maybe_git_hash {
+			format!("({} {})", git_hash, env!("GIRT_BUILD_DATE"))
+		}
+		else {
+			format!("({})", env!("GIRT_BUILD_DATE"))
+		};
 		assert!(version.starts_with("interactive-rebase-tool"));
 		assert!(version.ends_with(expected_meta.as_str()));
 	}
